9ef519db03c381ccdd985710afddfc9e3bf43be0
Ivan Sambrana <isambrana@invico.gov.ar>
2019-08-26 12:07:55 -0300

    first commit

---
+++ customer_8.java
@@ -0,0 +1,39 @@
+class Customer {
+    private String _name;
+    private Vector _rentals = new Vector();
+    public Customer(String name) {
+        _name = name;
+    };
+    public void addRental(Rental arg) {
+        _rentals.addElement(arg);
+    }
+    public String getName() {
+        return _name;
+    };
+
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Enumeration rentals = _rentals.elements();
+        String result = "Rental Record for " + getName() + "\n";
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            // add frequent renter points
+           frequentRenterPoints += each.getFrequentRenterPoints();
+           
+            result += "\t" + each.getMovie().getTitle() + "\t" +
+                each.getCharge() + "\n";
+            totalAmount += each.getCharge();
+        }
+        //add footer lines
+        result += "Amount owed is " + String.valueOf(totalAmount) +
+            "\n";
+        result += "You earned " + String.valueOf(frequentRenterPoints) +
+            " frequent renter points";
+        return result;
+    }
+
+ 
+
+}
+
